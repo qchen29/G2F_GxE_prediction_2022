7c8c08a86e897adc25888e77c96088a1c7148ab5
qchen33 <130582390+qchen33@users.noreply.github.com>
2023-06-14 15:52:08 -0500

    Add files via upload

---
+++ 4.2 Calculate_GxE_covariates.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+use strict;
+
+open IN, "./Prediction_Datasets/G2F_FullMarker_PC10_EC100.csv" or die;
+open OUT, "+>./Prediction_Datasets/G2F_FullMarker_PC10xEC100.csv" or die;
+
+my $head = <IN>;
+chomp $head;
+my @head = split /,/, $head;
+
+my @new_head;
+for(my $a=0; $a<100 ; $a++) {
+	for(my $b=100; $b<$#head+1; $b++) {
+		my $new_head = $head[$a] . "_" . $head[$b];
+		push(@new_head, $new_head);
+	}
+}
+my $out_head = join ",", @new_head;
+print OUT "$out_head\n";
+
+
+my $n = 0;
+while(<IN>) {
+	chomp;
+	my @aa = split /,/, $_;
+	my @new = ();
+	for(my $i=0; $i<100 ; $i++) {
+		for(my $j=100; $j<$#aa+1; $j++) {
+			my $new = $aa[$i] * $aa[$j];
+			push(@new, $new);
+		}
+	}
+
+	my $out = join ",", @new;
+	print OUT "$out\n";
+
+	$n++;
+	print "$n\n";
+}
+
+
